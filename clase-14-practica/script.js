d84ad906d751b2b2e1763fc21503e4593f277ed0
Lourdes.s <lou_santillan98@hotmail.com>
2024-06-07 17:49:13 -0300

    practica clase 14

---
+++ clase-14-practica/script.js
@@ -0,0 +1,46 @@
+/* mostrar por alerta el conteo de 1 al 10 (se deben ejecutar 10 alertas) */
+
+/* for(let iterador = 1; iterador <= 10; iterador = iterador + 1){
+    alert(iterador)
+}
+
+
+
+ */
+/* Solicitar al usuario un numero 3 veces, mostrar al final de las solicitudes una alerta que muestre la sumatoria de los 3 numeros 
+
+dime un numero:3
+dime un numero:3
+dime un numero:4
+alert> 10
+*/
+
+/* let sumatoria = 0
+
+for(let i = 1; i <= 3; i = i + 1){
+let numero = prompt('ingrese un numero')
+while(!numero || isNaN(numero)) {
+    numero = prompt('ERROR: DATO NO VALIDO')
+} 
+numero = Number (numero)
+sumatoria = sumatoria + numero 
+}
+
+alert ('resultado: ' + sumatoria)
+ */
+
+
+
+/* Solicitar al usuario la cantidad de personas en la clase (numero)
+Dependiendo de la cantidad de alumnos de la clase van a solicitar a cada uno el nombre
+
+(si la cantidad de personas de la clase es 5, van a solicitar 5 nombres
+
+    lueg de solicitar los 5 nombres mostrarlos en este formato:
+    lista alumnos:
+    -juan
+    -Pepe
+    -maria 
+    -carla 
+    -julieta 
+     */
